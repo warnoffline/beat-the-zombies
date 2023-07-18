3a0ac4f9f79138dca57c1a051e9066ccfa667014
warnoffline <94242401+warnoffline@users.noreply.github.com>
2023-07-18 16:22:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,34 +15,63 @@ function StartGame(){
         const timerDisplay = document.getElementById('timerDisplay');
         timerDisplay.textContent = rTime;
     }
-    function increaseScore() {
+
+    function plusScore() {
         score++;
-        const scoreEl = document.getElementById('scoreDisplay')
         scoreEl.textContent = score;
-        const activeCell = document.querySelector('.kletka.active');
+        scoreEl.classList.remove('red')
+        scoreEl.classList.add('green')
+        const activeCell = document.querySelector('.kletka.zombie');
         if (activeCell) {
-            activeCell.classList.remove('active');
+            activeCell.classList.add('zdanie')
+            activeCell.classList.remove('zombie');
+        }
+    }
+    function minusScore(){
+        score--;
+        scoreEl.textContent = score;
+        scoreEl.classList.remove('green')
+        scoreEl.classList.add('red')
+        const activeCell = document.querySelector('.kletka.people');
+        if (activeCell) {
+            activeCell.classList.add('zdanie')
+            activeCell.classList.remove('people');
         }
     }
     
-    function RandomCell() {
+    function RandomZombie() {
         const kletka = document.querySelectorAll('.kletka');
         kletka.forEach((cell) => {
-            cell.classList.remove('active');
+            cell.classList.remove('zombie');
+            cell.classList.remove('people');
+            cell.classList.add('zdanie')
         });
         let index = Math.floor(Math.random() * kletka.length);
-        kletka[index].classList.add('active');
-    
+        let zombieOrPeople = Math.floor(Math.random()*2)
+        if(zombieOrPeople === 0){
+            kletka[index].classList.remove('zdanie');
+            kletka[index].classList.add('zombie');
+        }
+        else{
+            kletka[index].classList.remove('zdanie');
+            kletka[index].classList.add('people');
+        }
+        
         kletka.forEach((el) => {
             el.addEventListener('click', () => {
-            if (el.classList.contains('active')) {
-                increaseScore();
+            if (el.classList.contains('zombie')) {
+                plusScore();
+            }
+            if (el.classList.contains('people')){
+                minusScore();
             }
             });
         });
     
         setTimeout(() => {
-            kletka[index].classList.remove('active');
+            kletka[index].classList.remove('zombie');
+            kletka[index].classList.remove('people')
+            kletka[index].classList.add('zdanie');
         }, 1000);
     
     
@@ -56,5 +85,5 @@ function StartGame(){
             game.style.display = 'inline'
         }
     }, 1000);
-    const interval = setInterval(RandomCell, 1000);
+    const interval = setInterval(RandomZombie, 1000);
 }
